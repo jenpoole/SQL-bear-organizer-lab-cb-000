6691784a30ed0f7bc1709af3c287bab54339002a
Jen Poole <contact.vandepool@gmail.com>
2018-02-14 03:38:59 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,8 +23,9 @@ def select_youngest_bear_and_returns_name_and_age
   "SELECT name, age FROM bears ORDER BY age ASC LIMIT 1;"
 end
 
+# selects the most prominent color and returns it with its count
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(color) DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
